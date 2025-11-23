889c917b914141b4e2cdc2e27fd19d493f7ff0b7
Melany Sonco <soncovillegasm@gmail.com>
2025-11-23 17:41:16 -0400

    feat: backend inicial funcionando con health endpoint

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+﻿const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'ok',
+    message: 'Backend funcionando desde GitHub CI/CD'
+  });
+});
+
+app.listen(port, () => {
+  console.log("Servidor escuchando en el puerto " + port);
+});
